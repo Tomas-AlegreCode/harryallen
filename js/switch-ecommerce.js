8ab7beec4ba059b518d4b441563c6b15fa648994
Tomas Alegre <tomasgonzalo.alegre@gmail.com>
2025-05-23 19:27:23 -0300

    cambio de bototes, cambio de estilo footer y comienzo de funciones en el toggle. Hay que seguir editando

---
+++ js/switch-ecommerce.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.career-banner input').forEach(radio => {
+  radio.addEventListener('change', function() {
+    if (this.id === 'journalist-option') {
+      // Redirige a la página de Journalist
+      window.location.href = "../index.html";
+    } else {
+      // Redirige a la página de E-commerce
+      window.location.href = "../pages/e-commerce.html";
+    }
+  });
+});
---
+++ js/toggle-ecommerce.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('.career-banner input').forEach(radio => {
+  radio.addEventListener('change', function() {
+    document.body.classList.toggle('ecommerce-mode', this.id === 'ecommerce-option');
+  });
+});
+
+document.getElementById('mode-toggle').addEventListener('change', function() {
+  if (this.checked) {
+    window.location.href = "../index.html"; // Redirige a E-commerce
+  } else {
+    window.location.href = "../pages/e-commerce.html"; // Redirige a Periodista
+  }
+});
